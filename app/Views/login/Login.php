77c4ed2a48452aba5831ba41f69c592cb3272c00
Felipe1291 <mapimcph@gmail.com>
2022-09-01 21:54:58 -0500

    login coregido

--- app/Views/login/Login.php
+++ app/Views/login/Login.php
@@ -7,30 +7,65 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<style>
+.gradient-custom {
+/* fallback for old browsers */
+background: #6a11cb;
 
+/* Chrome 10-25, Safari 5.1-6 */
+background: -webkit-linear-gradient(to right, rgba(106, 17, 203, 1), rgba(37, 117, 252, 1));
+
+/* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+background: linear-gradient(to right, rgba(106, 17, 203, 1), rgba(37, 117, 252, 1))
+}
+</style>
     <title>Login</title>
   </head>
   <body>
-    <div class="container">
-      
-      <div class="row">
-        <div class="col-sm-4"></div>
-        <div class="col-sm-4">
-            <br><br><br><br><br><br><br><br>
-            
-          <h1>Login</h1>
-          <form action="<?php echo base_url('/login') ?>" method="POST">
-            <label for="usuario">Usuario</label>
-            <input type="text" name="usuario" class="form-control" required="">
-            <label for="password">Password</label>
-            <input type="password" name="password" class="form-control" required="">
-            <br>
-            <button class="btn btn-primary">Entrar</button>
-          </form>
+  <section class="vh-100 gradient-custom">
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+        <div class="card bg-dark text-white" style="border-radius: 1rem;">
+          <div class="card-body p-5 text-center">
+
+            <div class="mb-md-5 mt-md-4 pb-5">
+<form>
+              <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
+              <p class="text-white-50 mb-5">Porfavor Ingrese su Usuario y Contraseña!</p>
+
+              <div class="form-outline form-white mb-4">
+                <input type="varchar" id="typeVarchar" class="form-control form-control-lg" />
+                <label class="form-label" for="typeVarchar">Usuario</label>
+              </div>
+
+              <div class="form-outline form-white mb-4">
+                <input type="password" id="typePasswordX" class="form-control form-control-lg" />
+                <label class="form-label" for="typePasswordX">Password</label>
+              </div>
+
+              <p class="small mb-5 pb-lg-2"><a class="text-white-50" href="#!">Forgot password?</a></p>
+
+              <button class="btn btn-outline-light btn-lg px-5" type="submit">Ingresar</button>
+
+              <div class="d-flex justify-content-center text-center mt-4 pt-1">
+                <a href="#!" class="text-white"><i class="fab fa-facebook-f fa-lg"></i></a>
+                <a href="#!" class="text-white"><i class="fab fa-twitter fa-lg mx-4 px-2"></i></a>
+                <a href="#!" class="text-white"><i class="fab fa-google fa-lg"></i></a>
+              </div>
+
+            </div>
+
+            <div>
+              </p>
+            </div>
+
+          </div>
         </div>
-        <div class="col-sm-4"></div>
       </div>
     </div>
+  </div>
+</section>
     
   </body>
 </html>
