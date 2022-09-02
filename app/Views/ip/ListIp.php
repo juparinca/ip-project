679f5f4f1f82467729cfe00a772342e88513044c
Alexander <astorga9323@hotmail.com>
2022-09-02 10:01:46 -0500

    Se crea modal el cual abre desde el botón editar

--- app/Views/ip/ListIp.php
+++ app/Views/ip/ListIp.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en">  
+    
 
 <head>
     <meta charset="UTF-8">
@@ -9,6 +10,7 @@
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+       
 </head>
 
 <body class="container-fluid mt-4">
@@ -50,7 +52,7 @@
             </tr>
         </thead>
         <tbody>
-            <?php foreach ($ips as $ip) { ?>
+            <?php foreach ($ips as $ip) { ?>    
             <td><?php echo $ip->ip; ?></td>
             <td><?php echo $ip->nomb_pais; ?></td>
             <td><?php echo $ip->num_reporte; ?></td>
@@ -59,8 +61,8 @@
             <td><?php echo $ip->fecha_bloqueo; ?></td>
             <td><?php echo $ip->estado; ?></td>
             <td><?php echo $ip->fecha_desbloqueo; ?></td>
-            <td>   
-            <th><button type="button" class="btn btn-primary">Editar</button></th>
+            <td>      
+            <th><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#mimodal">Editar</button></th>  
             <th><button type="button" class="btn btn-info">Desbloqueo</button></th>
             </td>
             </tr>
@@ -68,6 +70,74 @@
 }?>
         </tbody>
     </table>
+
+
+<!-- Codigo del Modal -->
+
+    <div class="modal fade" id="mimodal" tabindex="-1" arai-hidden="true" aria-labelledby="modalTitle" data-bs-backdrop="static">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Network Monitoring</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+
+        <p>
+        <form class="row g-3">
+  <div class="col-md-6">
+    <label for="inputEmail4" class="form-label">IP</label>
+    <input type="email" class="form-control" id="inputEmail4">
+  </div>
+  <div class="col-md-6">
+    <label for="inputPassword4" class="form-label">Pais</label>
+    <input type="password" class="form-control" id="inputPassword4">
+  </div>
+  
+  <div class="col-md-6">
+    <label for="inputPassword4" class="form-label">#Reporte</label>
+    <input type="password" class="form-control" id="inputPassword4">
+  </div>
+
+  <div class="col-md-6">
+    <label for="inputState" class="form-label">Categoria</label>
+    <select id="inputState" class="form-select">
+      <option selected>Choose...</option>
+      <option>...</option>
+    </select>
+  </div>
+  
+  <div class="col-6">
+    <label for="inputAddress2" class="form-label">Fecha Reporte</label>
+    <input type="date" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+  </div>
+  <div class="col-6">
+    <label for="inputAddress2" class="form-label">Fecha Bloqueo</label>
+    <input type="date" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+  </div>
+  <div class="col-md-6">
+    <label for="inputState" class="form-label">Estado</label>
+    <select id="inputState" class="form-select">
+      <option selected>Choose...</option>
+      <option>...</option>
+    </select>
+  </div>
+  
+</form>
+
+        </p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+
 </body>
 
 </html>
