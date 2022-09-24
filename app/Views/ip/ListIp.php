e90e70f7d938b679ed9a847695580e1b4fcb85b8
Bianey <bianey000@gmail.com>
2022-09-23 21:56:47 -0500

    Revert "Update ListIp.php"
    This reverts commit 1822a57e7ea8d6d622ad9726757861dd2d5fbcbe.

--- app/Views/ip/ListIp.php
+++ app/Views/ip/ListIp.php
@@ -4,10 +4,6 @@
  
 
 <head>
-    
-<script type="text/javascript" src="https://cdn.datatables.net/v/bs4/dt-1.10.20/datatables.min.js"></script>  
-<script type="text/javascript" src="https://cdn.datatables.net/scroller/2.0.2/js/dataTables.scroller.min.js"></script> 
-
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -15,7 +11,7 @@
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-    <!--<link rel="stylesheet" type="text/css" href="estilo.css">-->
+       
 </head>
 
 <body class="container-fluid mt-4">
@@ -69,19 +65,12 @@
             <td><?php echo $ip->estado; ?></td>
             <td><?php echo $ip->fecha_desbloqueo; ?></td>
             <td>   
-              
-            
-            
+                     
             <th><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#mimodal">Editar</button></th>  
             <th><button type="button" class="btn btn-info">Desbloquear</button></th>
             </td>
             </tr>
             <?php
             }?>
-<!--
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
--->
         </tbody>
     </table>
